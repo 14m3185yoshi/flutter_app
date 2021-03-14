efd88f1eaa1c83a18e14074c8147ff7cab39d988
西脇佳宏 <nishiwakiyoshihiro@nishiwakiyoshihironoMacBook-Air.local>
2021-03-14 13:54:19 +0900

    [add]画面遷移(スタックに記録されているものを使って)

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Mypage()));
+void main() => runApp(MaterialApp(home: FirstRoute()));
 
-class Mypage extends StatefulWidget{
-  @override
-  _MyPageState createState() => _MyPageState();
-}
-
-class _MyPageState extends State{
-  int _counter = 0;
-  void _incrementCounter(){
-    setState(() => _counter++);
-  }
+class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("push button")),
-      body: Center(child: Text("$_counter")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+      appBar: AppBar(
+        title: Text('First Router'),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                Navigator.push(context,//Navigator.pushが画面に遷移するためのメソッド。一つ目のcontextはひとまずおまじないと覚えておく。
+                  MaterialPageRoute(//ここからが画面の遷移先になる。このMaterialPageRouteは画面遷移の方法を指定するWidgetで、Android風のアニメーションで画面を切り替えてくれる。
+                      builder: (context) => SecondRoute()
+                ));
+              },
+              child: Text('Push Here',
+                style: TextStyle(fontSize: 20),
+            )),
+          ]
+        )
+      )
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Route'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Push Here',
+                style: TextStyle(fontSize: 20),
+            )),
+          ]
+        )
+      )
     );
   }
 }
